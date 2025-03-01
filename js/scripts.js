6fb75c950e3977d03df7f698c777da70069f9afe
Cory Nelson <34819470+NelsonCory@users.noreply.github.com>
2025-03-01 00:41:02 -0600

    determining text fragment, loading json

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,23 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
+fetch('./data/blogs.json')
+  .then(response => response.json())
+  .then(data => {
+    // Use the JSON data here
+    console.log(data);
+  })
+  .catch(error => console.error('Error loading JSON:', error));
 
 function loadBlog(){
+    
     document.getElementById("pageTitle").innerHTML = "No Data Found";
+    console.log(window.location.href);
+    console.log(grabFragment());
+}
+
+function grabFragment(){
+    var url = window.location.href;
+    url = url.split("#")[1];
+    return url
 }
